e5ccf2beddc138b1d64eb889d759b6d93caab974
unknown <ayoolabode22@gmail.com>
2023-02-27 23:24:20 -0700

    added the chat menu to work kinda

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,35 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLogOut } from "../hooks/useLogOut";
 import { useAuthContext } from "../hooks/useAuthContext";
 import "../Home.css";
 
-
 export default function Home() {
   const { logOut } = useLogOut();
   const { user } = useAuthContext();
-  const handleClick = () => {
-    logOut();
+  const [message, setMessage] = useState("");
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Message submitted: ${message}`);
+    setMessage("");
   };
 
   return (
-    <div class="parent">
-    <div class="top">
-        <div class="group-container">
-            <h1>gROUPS GO HERE</h1>
+    <div className="parent">
+      <div className="top">
+        <div className="group-container">
+          <h1>Groups</h1>
         </div>
-    </div>
-    <div class="main">
-        <div class="chats">
-        <h1>chats GO HERE</h1>
+      </div>
+      <div className="main">
+        <div className="chats">
+          <h1>Chats</h1>
         </div>
-        <div class="message-area">
-        <h1>msgs GO HERE</h1>
-        <h1>msgs GO HERE</h1>
+        <div className="chat-chatbox-area">
+          <div className="message-area">
+            <div className="message-container">
+              <h1>First MESSAGE</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>Stuff</h1>
+              <h1>LAST mesSAGE</h1>
+            </div>
+          </div>
+          <div className="chat-box">
+            <form onSubmit={handleMessageSubmit}>
+              <input
+                type="text"
+                placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-    <div class="bot">
-    <h1>Footer</h1>
-    </div>
-</div>
   );
 }
