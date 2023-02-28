a531788bd6cf3e6663979a4246b8426a59b1963c
unknown <ayoolabode22@gmail.com>
2023-02-27 21:36:37 -0700

    added some middleware

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { getAllUsers } = require("../controllers/user-controller");
-const { createUser } = require("../controllers/user-controller");
-const { updateUser } = require("../controllers/user-controller");
-const { deleteUser } = require("../controllers/user-controller");
-const { getUserById } = require("../controllers/user-controller");
-const { signUpUser } = require("../controllers/user-controller");
-const { loginUser } = require("../controllers/user-controller");
+// const { getAllUsers } = require("../controllers/user-controller");
+// const { createUser } = require("../controllers/user-controller");
+// const { updateUser } = require("../controllers/user-controller");
+// const { deleteUser } = require("../controllers/user-controller");
+// const { getUserById } = require("../controllers/user-controller");
+// const { signUpUser } = require("../controllers/user-controller");
+// const { loginUser } = require("../controllers/user-controller");
+
+const {
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserById,
+  signUpUser,
+  loginUser,
+} = require("../controllers/user-controller");
 
 const router = express.Router();
 
