9b430c8529f940186bb3823dd12f861fc8cadc81
unknown <ayoolabode22@gmail.com>
2023-03-09 14:02:27 -0700

    gaping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/LogIn";
 import Home from "./pages/Home";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={user ? <Navigate to="/" /> : <SignUp />} />
+      <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<h1>404 Not Found</h1>} /> 
+      <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
 }
