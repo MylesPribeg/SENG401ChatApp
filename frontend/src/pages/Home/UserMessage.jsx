bca80354b6280c87880b55ec43ab41769e28eb7c
ukefat <ukefat@gmail.com>
2023-03-23 21:51:25 -0600

    added vars for UserMessage component and ruined userMessage css

--- frontend/src/pages/Home/UserMessage.jsx
+++ frontend/src/pages/Home/UserMessage.jsx
@@ -3,8 +3,10 @@ import avater from "../../assets/avater.svg";
 export default function userMessage(props) {
   return (
     <div className="message-div">
-       <img src={avater} alt="" />
+      <img src={avater} alt="" />
+      <h2 className="message-uname">{props.username}</h2>
       <h1 className="message-text">{props.message}</h1>
+      <h3 className="message-time">{props.time}</h3>
     </div>
   );
 }
