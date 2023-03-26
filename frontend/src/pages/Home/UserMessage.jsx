d1fe08f5e6c840db6d5b11f389a325f3a286b343
Muhammad-I-Hasan <101440470+Muhammad-I-Hasan@users.noreply.github.com>
2023-03-25 23:56:24 -0600

    added back user message change

--- frontend/src/pages/Home/UserMessage.jsx
+++ frontend/src/pages/Home/UserMessage.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import avater from "../../assets/avater.svg";
+import "./UserMessage.css"
+// export default function userMessage(props) {
+//   return (
+//     <div className="message-div">
+//       <img src={avater} alt="" />
+//       <h2 className="message-uname">{props.username}</h2>
+//       <h1 className="message-text">{props.message}</h1>
+//       <h3 className="message-time">{props.time}</h3>
+//     </div>
+//   );
+// }
+
 export default function userMessage(props) {
   return (
-    <div className="message-div">
-       <img src={avater} alt="" />
-      <h1 className="message-text">{props.message}</h1>
+    
+    <div className="messageCard">
+      <div className="avatar"><img src={avater} alt=""/></div>
+      
+      <div className="messageContainer">
+        
+        <div className="messageTop"> 
+          <h2 className="messageSender">Username</h2>
+          <h4 className="messageTime">2 Hours ago</h4>
+        </div>
+
+        <h3 className="messageContent">
+        {props.message}
+        </h3>
+
+      </div>
+
     </div>
   );
 }
+
