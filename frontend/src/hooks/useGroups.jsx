f805672e8717a7fa37506249db90fe4ccfd35438
unknown <ayoolabode22@gmail.com>
2023-03-25 20:54:25 -0600

    fixed double messeges issue

--- frontend/src/hooks/useGroups.jsx
+++ frontend/src/hooks/useGroups.jsx
@@ -1,68 +1,55 @@
-import { useReducer, useState } from "react"
-
+import { useReducer, useState } from "react";
 
 const groupReducer = (state, action) => {
-    switch(action.type){
-        case "ADDGROUP":
-            return[...state, {id:4,name:action.name,messages:[]}]
-        case "ADDMESSAGE":
-           console.log("before: " + state[action.idx].messages )
-           state[action.idx].messages = [...state[action.idx].messages, action.msg]
-           console.log("after " + state[action.idx].messages)
-           return state
-        case "DELETEGROUP":
-            // return []
-            return state.splice(action.idx,1)
-        case "GROUPCLICKED":
-            // state[0].active=false
-            
-            state[action.idx].active=true
-            state[action.prevIdx].active=false
-            return state
+  switch (action.type) {
+    case "ADDGROUP":
+      return [...state, { id: 4, name: action.name, messages: [] }];
+    case "ADDMESSAGE":
+      console.log("before: " + state[action.idx].messages);
+      const newState = state.map((group, index) => {
+        if (index === action.idx) {
+          return { ...group, messages: [...group.messages, action.msg] };
+        }
+        return group;
+      });
+      console.log("after " + newState[action.idx].messages);
+      return newState;
+    case "DELETEGROUP":
+      // return []
+      return state.splice(action.idx, 1);
+    case "GROUPCLICKED":
+      // state[0].active=false
 
-        default:
-            return state
-    }
-    
-}
+      state[action.idx].active = true;
+      state[action.prevIdx].active = false;
+      return state;
 
-export const useGroups = ()=>{
-    
+    default:
+      return state;
+  }
+};
 
-    const [groupsState, groupsStateDispatch] = useReducer(  groupReducer, [
+export const useGroups = () => {
+  const [groupsState, groupsStateDispatch] = useReducer(groupReducer, [
+    {
+      id: 0,
+      active: true,
+      name: "hello world",
+      messages: ["dummy message 1", "dummy message 2"],
+    },
+    {
+      id: 1,
+      active: false,
+      name: "test",
+      messages: ["dummy message 1", "poggers"],
+    },
+    {
+      id: 2,
+      active: false,
+      name: "not pog",
+      messages: ["hasan is a monkey monkey"],
+    },
+  ]);
 
-        {
-          id:0,
-          active:true,
-          name:"hello world",
-          messages:[
-            "dummy message 1",
-            "dummy message 2"
-          ]
-        },{
-          id:1,
-          active:false,
-          name:"test",
-          messages:[
-            "dummy message 1",
-            "poggers"
-          ]
-        },{
-          id:2,
-          active:false,
-          name:"not pog",
-          messages:[
-            "hasan is a monkey monkey"
-          ]
-        },
-      ])
-      
-      
-
-      return {groupsState,groupsStateDispatch}
-    
-
-
-
-
-}
+  return { groupsState, groupsStateDispatch };
+};
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -10,7 +10,6 @@ import { useColour } from "../../hooks/useColour";
 import { Box } from "@mui/system";
 import { useGroups } from "../../hooks/useGroups";
 
-
 //fsdfsd
 import { useGroupsContext } from "../../hooks/useGroupsContext";
 import { useEffect } from "react";
@@ -20,159 +19,139 @@ export default function Home() {
   const { user } = useAuthContext();
   const [message, setMessage] = useState("");
 
-
   const navigate = useNavigate();
 
-  const {getBackGroundColor} = useColour()
+  const { getBackGroundColor } = useColour();
 
   // const [currentActiveGroupIndex, setCurrentActiveGroupIndex] = useState(0)
-  const {groupsState,groupsStateDispatch} = useGroups()
-  const [activeIdx, setActiveIdx] = useState(0)
+  const { groupsState, groupsStateDispatch } = useGroups();
+  const [activeIdx, setActiveIdx] = useState(0);
 
   const username = user?.username;
-  
+
   const handleMessageSubmit = (e) => {
     e.preventDefault();
-    groupsStateDispatch({type:"ADDMESSAGE", idx:activeIdx,msg:message})
+    groupsStateDispatch({ type: "ADDMESSAGE", idx: activeIdx, msg: message });
     setMessage("");
 
     // setMessages([...messages, message]);
-    
   };
 
   const handleSettingsClick = () => {
-    navigate("/settings")
+    navigate("/settings");
   };
 
   const renderGroups = (groups) => {
     return groups.map((group) => {
-      return <Group key={group._id} val={group} clickHandler={ () => {
-        // console.log("group index pressed: " + index + "previously active index " +  currentActiveGroupIndex)
-        // groups[currentActiveGroupIndex].active=false
-        groupsStateDispatch({type:"GROUPCLICKED",idx:index,prevIdx:activeIdx})
-        setActiveIdx(index)
-        // groups[index].active=true
-        // setCurrentActiveGroupIndex(index)
-
-
-      }
-
-      }></Group>
-    })
-  }
-//---------------------------------//---------------------------------//---------------------------------//---------------------------------//---------------------------------
-  const {groups, dispatch} = useGroupsContext()
+      return (
+        <Group
+          key={group._id}
+          val={group}
+          clickHandler={() => {
+            // console.log("group index pressed: " + index + "previously active index " +  currentActiveGroupIndex)
+            // groups[currentActiveGroupIndex].active=false
+            groupsStateDispatch({
+              type: "GROUPCLICKED",
+              idx: index,
+              prevIdx: activeIdx,
+            });
+            setActiveIdx(index);
+            // groups[index].active=true
+            // setCurrentActiveGroupIndex(index)
+          }}
+        ></Group>
+      );
+    });
+  };
+  //---------------------------------//---------------------------------//---------------------------------//---------------------------------//---------------------------------
+  const { groups, dispatch } = useGroupsContext();
 
   useEffect(() => {
-    
     const fetchGroups = async () => {
-      const response = await fetch("http://localhost:8000/groups/")
+      const response = await fetch("http://localhost:8000/groups/");
       const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: "SET_GROUPS", payload: json})
+        dispatch({ type: "SET_GROUPS", payload: json });
       }
-    }
+    };
 
-    fetchGroups()
-  }, [dispatch])
-//---------------------------------//---------------------------------//---------------------------------//---------------------------------
-
+    fetchGroups();
+  }, [dispatch]);
+  //---------------------------------//---------------------------------//---------------------------------//---------------------------------
 
   return (
-    <Box className="parent" style={{backgroundColor:getBackGroundColor()}}
-      
-    
-    >
+    <Box className="parent" style={{ backgroundColor: getBackGroundColor() }}>
       <Box className="top">
         <Box className="groups">
-          {groups && groups.map((group) => {
-            //<Group key={group._id} group={group}/>
-            <h1>Deez</h1>
-            console.log(group);
-          })}
-          {
-            groups && renderGroups(groups)
-          }
+          {groups &&
+            groups.map((group) => {
+              //<Group key={group._id} group={group}/>
+              <h1>Deez</h1>;
+              console.log(group);
+            })}
+          {groups && renderGroups(groups)}
         </Box>
         <Box>
           <button> add group </button>
         </Box>
-
-
       </Box>
-      <Box className="body"
-      sx={{
-    
-
-      }}  
-      >
-        <Box className="sideview" sx={{
-          backgroundColor:"red",
-          flex:1
-          
-        }}>
-          <Box className="users" sx={{
-
-            
-          }}>
+      <Box className="body" sx={{}}>
+        <Box
+          className="sideview"
+          sx={{
+            backgroundColor: "red",
+            flex: 1,
+          }}
+        >
+          <Box className="users" sx={{}}>
             <div>user1</div>
-            
-
           </Box>
           <Box className="optionPlaceHolder">
+            <p className="username">{username}</p>
 
-          <p className="username">{username}</p>
-          
-          <img
-            className="settings-svg"
-            onClick={handleSettingsClick}
-            src={settingssvg}
-            alt=""
-          />
-          <button onClick={logOut}>Log Out</button>
-
+            <img
+              className="settings-svg"
+              onClick={handleSettingsClick}
+              src={settingssvg}
+              alt=""
+            />
+            <button onClick={logOut}>Log Out</button>
           </Box>
         </Box>
 
-        <Box className="textArea chat-chatbox-area" sx={{
-          backgroundColor:"white",
-          flex:3
-        }}>
-
-            
-            <div className="message-area">
+        <Box
+          className="textArea chat-chatbox-area"
+          sx={{
+            backgroundColor: "white",
+            flex: 3,
+          }}
+        >
+          <div className="message-area">
             <div className="chats">
               <p>Chats</p>
             </div>
-              <div className="message-container">
-                {groupsState[activeIdx].messages.map((message, index) => (
-                  <UserMessage key={index} message={message} />
-                ))}
-              </div>
+            <div className="message-container">
+              {groupsState[activeIdx].messages.map((message, index) => (
+                <UserMessage key={index} message={message} />
+              ))}
             </div>
-            <div className="chat-box">
-              <form onSubmit={handleMessageSubmit}>
-                <input
-                  type="text"
-                  placeholder="Type a message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-                <button type="submit">Send</button>
-              </form>
-            </div>
-          
-
+          </div>
+          <div className="chat-box">
+            <form onSubmit={handleMessageSubmit}>
+              <input
+                type="text"
+                placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </Box>
-
-
-        
       </Box>
 
-
-
-{/*         
+      {/*         
         <div className="groups">
           <Group />
           <Group />
@@ -220,7 +199,6 @@ export default function Home() {
             </form>
           </div>
         </div> */}
-      
     </Box>
   );
 }
